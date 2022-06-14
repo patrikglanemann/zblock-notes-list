6a7b3f3b6fd97d14253ca93844f09999ed9400eb
PatrikG <patrik0gn@gmail.com>
2022-06-14 21:11:53 +0200

    Fix show itself when vault loads

--- main.ts
+++ main.ts
@@ -1,42 +1,38 @@
 import { Plugin } from "obsidian";
 export default class ExamplePlugin extends Plugin {
 	async onload() {
-		this.registerMarkdownCodeBlockProcessor(
-			"zblockNotes",
-			(content, el, ctx) => {
-				const files = this.app.vault.getMarkdownFiles().map((file) => ({
-					name: file.basename,
-					lastChanged: file.stat.mtime,
-					path: file.path,
-				}));
+		this.app.workspace.onLayoutReady(async () => {
+			this.registerMarkdownCodeBlockProcessor(
+				"zblockNotes",
+				(content, el, ctx) => {
+					const files = this.app.vault
+						.getMarkdownFiles()
+						.map((file) => ({
+							name: file.basename,
+							lastChanged: file.stat.mtime,
+							path: file.path,
+						}));
 
-				let activeFile: string;
-
-				try {
-					activeFile = this.app.workspace.getActiveFile().path;
-				} catch {
-					console.warn(
-						"could not read path of active file. Active file not loaded yet."
+					const activeFile = this.app.workspace.getActiveFile().path;
+					const filesF = files.filter(
+						(file) => file.path != activeFile
 					);
-				}
 
-				const filesF = files.filter((file) => file.path != activeFile);
-
-				filesF.sort((a, b) => {
-					return b.lastChanged - a.lastChanged;
-				});
-
-				const baseDiv = el.createDiv("div");
-
-				filesF.map((file) => {
-					const block = baseDiv.createEl("blockquote");
-					block.createEl("h4", { text: file.name });
-					block.createEl("span", {
-						attr: { src: file.path },
-						cls: "internal-embed",
+					filesF.sort((a, b) => {
+						return b.lastChanged - a.lastChanged;
 					});
-				});
-			}
-		);
+
+					const baseDiv = el.createDiv("div");
+					filesF.map((file) => {
+						const block = baseDiv.createEl("blockquote");
+						block.createEl("h4", { text: file.name });
+						block.createEl("span", {
+							attr: { src: file.path },
+							cls: "internal-embed",
+						});
+					});
+				}
+			);
+		});
 	}
 }
