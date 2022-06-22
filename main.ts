f2b359976dbd0f3ebe8ab9e42e90f20f56d6d5bb
PatrikG <patrik0gn@gmail.com>
2022-06-22 23:04:17 +0200

    Add limit, link and change element create order

--- main.ts
+++ main.ts
@@ -1,38 +1,56 @@
 import { Plugin } from "obsidian";
-export default class ExamplePlugin extends Plugin {
+
+export default class ZblockNotes extends Plugin {
 	async onload() {
-		this.app.workspace.onLayoutReady(async () => {
-			this.registerMarkdownCodeBlockProcessor(
-				"zblockNotes",
-				(content, el, ctx) => {
-					const files = this.app.vault
-						.getMarkdownFiles()
-						.map((file) => ({
-							name: file.basename,
-							lastChanged: file.stat.mtime,
-							path: file.path,
-						}));
-
-					const activeFile = this.app.workspace.getActiveFile().path;
-					const filesF = files.filter(
-						(file) => file.path != activeFile
-					);
-
-					filesF.sort((a, b) => {
-						return b.lastChanged - a.lastChanged;
-					});
-
-					const baseDiv = el.createDiv("div");
-					filesF.map((file) => {
-						const block = baseDiv.createEl("blockquote");
-						block.createEl("h4", { text: file.name });
-						block.createEl("span", {
-							attr: { src: file.path },
-							cls: "internal-embed",
-						});
-					});
+		this.registerMarkdownCodeBlockProcessor(
+			"zblockNotes",
+			(content, el, ctx) => {
+				let limit = 10;
+				let inputLimit = parseInt(content);
+				if (!isNaN(inputLimit)) {
+					limit = inputLimit;
 				}
-			);
-		});
+
+				const files = this.app.vault
+					.getMarkdownFiles()
+					.map((file) => ({
+						name: file.basename,
+						lastChanged: file.stat.mtime,
+						path: file.path,
+					}))
+					.filter((file) => file.path != ctx.sourcePath)
+					.sort((a, b) => {
+						return b.lastChanged - a.lastChanged;
+					})
+					.slice(0, limit);
+
+				const baseDiv = document.createElement("div");
+
+				files.map((file) => {
+					const block = baseDiv.createEl("blockquote", {
+						cls: "zblockNotes",
+					});
+					const link = block.createEl("a", {
+						cls: "internal-link zblockNotes-link",
+						href: file.path,
+						attr: {
+							"data-href": file.path,
+							target: "_blank",
+							rel: "noopener",
+						},
+					});
+					link.createEl("h6", {
+						cls: "zblockNotes-title",
+						text: file.name,
+					});
+					block.createEl("span", {
+						attr: { src: file.path },
+						cls: "internal-embed",
+					});
+				});
+
+				el.appendChild(baseDiv);
+			}
+		);
 	}
 }
